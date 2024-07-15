9ac3b0d9e91209f9c3ef713e5bd0839fdc12077f
lijinhong11 <tygfhk@outlook.com>
2024-07-15 11:35:08 +0800

    init

---
+++ src/main/java/org/irmc/industrialrehabilitation/IndustrialRehabilitation.java
@@ -0,0 +1,17 @@
+package org.irmc.industrialrehabilitation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IndustrialRehabilitation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
