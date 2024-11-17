350f4bbc7a8dbec03b0019329c97dc85d9c9bb51
balugaq <129668183+balugaq@users.noreply.github.com>
2024-11-17 23:33:27 +0800

    118种元素

---
+++ src/main/java/org/irmc/industrialrevival/api/elements/ElementUtils.java
@@ -0,0 +1,157 @@
+package org.irmc.industrialrevival.api.elements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ElementUtils {
+    public static final int UNKNOWN = Integer.MIN_VALUE;
+    public static ElementType getBySymbol(String s) {
+        try {
+            return ElementType.valueOf(s.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    public static ElementType getByRelativeAtomicMass(double mass) {
+        for (ElementType e : ElementType.values()) {
+            if (Math.abs(e.getRelativeAtomicMass() - mass) < 0.001) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public static List<ElementType> getByRelativeAtomicMass(double min, double max) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (e.getRelativeAtomicMass() >= min && e.getRelativeAtomicMass() <= max) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static ElementType getByProtonNumber(int protonNumber) {
+        for (ElementType e : ElementType.values()) {
+            if (e.getProtonNumber() == protonNumber) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public static ElementType getByNeutronNumber(int neutronNumber) {
+        for (ElementType e : ElementType.values()) {
+            if (e.getNeutronNumber() == neutronNumber) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public static List<ElementType> getByValence(int... valences) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (Arrays.equals(e.getValence().getValences(), valences)) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByGroup(ElementType.ElementGroup group) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (e.getElementGroup() == group) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByPeriod(int period) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (e.getPeriod() == period) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByIsMetal(boolean isMetal) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (e.isMetal() == isMetal) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByMeltingPoint(double meltingPoint) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (Math.abs(e.getMeltingPoint() - meltingPoint) < 0.001) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByMeltingPoint(double min, double max) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (e.getMeltingPoint() >= min && e.getMeltingPoint() <= max) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByBoilingPoint(double boilingPoint) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (Math.abs(e.getBoilingPoint() - boilingPoint) < 0.001) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByBoilingPoint(double min, double max) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (e.getBoilingPoint() >= min && e.getBoilingPoint() <= max) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByDensity(double density) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (Math.abs(e.getDensity() - density) < 0.001) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public static List<ElementType> getByDensity(double min, double max) {
+        List<ElementType> elements = new ArrayList<>();
+        for (ElementType e : ElementType.values()) {
+            if (e.getDensity() >= min && e.getDensity() <= max) {
+                elements.add(e);
+            }
+        }
+        return elements;
+    }
+
+    public List<ElementType> getAllElements() {
+        return Arrays.asList(ElementType.values());
+    }
+}
