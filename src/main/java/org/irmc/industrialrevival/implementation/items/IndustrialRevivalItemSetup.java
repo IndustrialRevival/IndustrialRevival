8e56f02386076f1497cc73c32e6bbedf259f5feb
balugaq <129668183+balugaq@users.noreply.github.com>
2024-11-19 20:53:47 +0800

    Update IndustrialRevivalItemSetup.java

--- src/main/java/org/irmc/industrialrevival/implementation/items/IndustrialRevivalItemSetup.java
+++ src/main/java/org/irmc/industrialrevival/implementation/items/IndustrialRevivalItemSetup.java
@@ -5,12 +5,35 @@ import org.irmc.industrialrevival.api.recipes.CraftMethod;
 import org.irmc.industrialrevival.api.recipes.RecipeType;
 import org.irmc.industrialrevival.implementation.IndustrialRevival;
 import org.irmc.industrialrevival.implementation.groups.IRItemGroups;
+import org.irmc.industrialrevival.implementation.items.debug.DebugHead;
 import org.irmc.industrialrevival.implementation.items.debug.Debugger;
+import org.irmc.industrialrevival.implementation.items.debug.Empty;
 
 public class IndustrialRevivalItemSetup {
     public static final IndustrialRevival INSTANCE = IndustrialRevival.getInstance();
     public static final ItemStack[] EMPTY_RECIPE = new ItemStack[] {};
 
+    public static final Empty EMPTY = new Empty()
+            .setAddon(INSTANCE)
+            .addItemGroup(IRItemGroups.DEBUG)
+            .setItemStack(IndustrialRevivalItems.EMPTY)
+            .addCraftMethod(item -> new CraftMethod(
+                    RecipeType.NULL,
+                    EMPTY_RECIPE,
+                    item
+            ))
+            .cast(Empty.class);
+
+    public static final DebugHead DEBUG_HEAD = new DebugHead()
+            .setAddon(INSTANCE)
+            .addItemGroup(IRItemGroups.DEBUG)
+            .setItemStack(IndustrialRevivalItems.DEBUG_HEAD)
+            .addCraftMethod(item -> new CraftMethod(
+                    RecipeType.NULL,
+                    EMPTY_RECIPE,
+                    item
+            ))
+            .cast(DebugHead.class);
     public static final Debugger DEBUGGER = new Debugger()
             .setAddon(INSTANCE)
             .addItemGroup(IRItemGroups.DEBUG)
@@ -23,6 +46,8 @@ public class IndustrialRevivalItemSetup {
             .cast(Debugger.class);
 
     public static void setup() {
+        EMPTY.register();
+        DEBUG_HEAD.register();
         DEBUGGER.register();
     }
 }
