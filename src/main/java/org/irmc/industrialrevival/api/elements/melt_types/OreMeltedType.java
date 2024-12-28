c37fc76ee86fbfbca74755b249ca0138dae71804
balugaq <129668183+balugaq@users.noreply.github.com>
2024-12-28 15:56:27 +0800

    change type

--- src/main/java/org/irmc/industrialrevival/api/elements/melt_types/OreMeltedType.java
+++ src/main/java/org/irmc/industrialrevival/api/elements/melt_types/OreMeltedType.java
@@ -2,17 +2,19 @@ package org.irmc.industrialrevival.api.elements.melt_types;
 
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
+import org.bukkit.NamespacedKey;
 import org.irmc.industrialrevival.api.elements.ElementType;
 import org.irmc.industrialrevival.api.elements.MeltedType;
 import org.irmc.industrialrevival.implementation.IndustrialRevival;
+import org.irmc.industrialrevival.utils.KeyUtil;
 
 @Getter
 public class OreMeltedType extends MeltedType {
-    private final Component identifier;
+    private final NamespacedKey identifier;
     private final Component name;
     private final Component meltedName;
     protected OreMeltedType(ElementType elementType) {
-        this.identifier = Component.text(elementType.name().toLowerCase());
+        this.identifier = KeyUtil.customKey("ore_melted_type_" + elementType.name().toLowerCase());
         // Didn't find a better way to get the name or melted name for ore melted types
         // todo: Should change getItemName to getText in the future
         this.name = IndustrialRevival.getInstance().getLanguageManager().getItemName("ore_melted_type_name_" + elementType.name().toLowerCase());
